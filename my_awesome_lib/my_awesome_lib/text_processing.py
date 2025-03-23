6b1e9ed7ade98395c69e12738f5e13dff5b11f0e
Tomasz Łagowski <qusmikus@onet.pl>
2025-03-23 23:08:52 +0100

    Initial commit

--- my_awesome_lib/my_awesome_lib/text_processing.py
+++ my_awesome_lib/my_awesome_lib/text_processing.py
@@ -4,15 +4,15 @@ import re
 
 def count_words(text):
     """
-    Zlicza liczbę słów w tekście, traktując słowo jako ciąg znaków oddzielony spacjami lub znakami interpunkcyjnymi.
-    
+    Zlicza liczbę słów w tekście, traktując słowo jako ciąg znaków alfanumerycznych oddzielony spacjami lub znakami interpunkcyjnymi.
+
     :param text: Tekst, w którym będą liczone słowa.
     :return: Liczba słów w tekście.
     """
-    # Usuwamy interpunkcję na końcu słów, ale nie w środku
-    text = re.sub(r'[^\w\s]', '', text)
-    words = text.split()
+    # Znajdujemy wszystkie słowa, ignorując interpunkcję
+    words = re.findall(r'\b\w+\b', text)
     return len(words)
+
 
 
 
