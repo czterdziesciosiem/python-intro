3ab5482a76e487f181847191999ac8dd35b9ef1d
Tomasz Łagowski <qusmikus@onet.pl>
2025-03-23 22:59:16 +0100

    Initial commit

--- my_awesome_lib/tests/test_text_processing.py
+++ my_awesome_lib/tests/test_text_processing.py
@@ -1,17 +1,12 @@
 # tests/test_text_processing.py
 import unittest
-from my_awesome_lib.text_processing import count_words, remove_special_chars
+from my_awesome_lib.text_processing import count_words
 
 class TestTextProcessing(unittest.TestCase):
-
     def test_count_words(self):
         self.assertEqual(count_words("To jest test."), 4)
-        self.assertEqual(count_words("Python!"), 1)
+        self.assertEqual(count_words("Hello world!"), 2)
+        self.assertEqual(count_words("Python 3.8"), 2)
+        self.assertEqual(count_words("test"), 1)
+        self.assertEqual(count_words(""), 0)
 
-    def test_remove_special_chars(self):
-        self.assertEqual(remove_special_chars("Python@#123!"), "Python123")
-        self.assertEqual(remove_special_chars("Hello, World!"), "Hello World")
-        
-if __name__ == '__main__':
-    unittest.main()
- 
